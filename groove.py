0582dd9ccd48e2a5c6425baf138592cc42e4a1be
boyska <piuttosto@logorroici.org>
2014-01-06 14:26:40 +0100

    Better logging and boring cleanup

--- groove.py
+++ groove.py
@@ -11,7 +11,7 @@ import subprocess
 import gzip
 import threading
 import logging
-from pprint import pprint, pformat
+from pprint import pformat
 
 if sys.version_info[1] >= 6:
     import json
@@ -42,23 +42,22 @@ h["country"]["CC4"] = 0
 h["country"]["ID"] = 57
 h["country"]["IPR"] = 0
 h["privacy"] = 0
-h["session"] = (''.join(random.choice(string.digits + string.letters[:6]) for x in range(32))).lower()
+h["session"] = (''.join(
+    random.choice(string.digits + string.letters[:6])
+    for x in range(32)
+)).lower()
 h["uuid"] = str.upper(str(uuid.uuid4()))
 
-#The string that is shown when the program loads
-entrystring = \
-"""A Grooveshark song downloader in python
-by George Stephanos <gaf.stephanos@gmail.com>
-"""
 
-#Generate a token from the method and the secret string (which changes once in a while)
+#Generate a token from the method and the secret string
+#(which changes once in a while)
 def prepToken(method, secret):
     rnd = (''.join(random.choice(string.hexdigits) for x in range(6))).lower()
     return rnd + hashlib.sha1('%s:%s:%s:%s' % (method, _token, secret, rnd)).hexdigest()
 
 #Fetch a queueID (right now we randomly generate it)
 def getQueueID():
-    return random.randint(10000000000000000000000,99999999999999999999999) #For now this will do
+    return random.randint(10000000000000000000000, 99999999999999999999999)
 
 #Get the static token issued by sharkAttack!
 def getToken():
@@ -193,8 +192,9 @@ def getStreamKeyFromSongIDs(id):
     conn.request("POST", "/more.php?" + p["method"], json.JSONEncoder().encode(p), jsqueue[3])
     return json.JSONDecoder().decode(gzip.GzipFile(fileobj=(StringIO.StringIO(conn.getresponse().read()))).read())["result"]
 
+
 #Add a song to the browser queue, used to imitate a browser
-def addSongsToQueue(songObj, songQueueID, source = "user"):
+def addSongsToQueue(songObj, songQueueID, source='user'):
     queueObj = {}
     queueObj["songID"] = songObj["SongID"]
     queueObj["artistID"] = songObj["ArtistID"]
@@ -213,7 +213,9 @@ def addSongsToQueue(songObj, songQueueID, source = "user"):
     conn.request("POST", "/more.php?" + p["method"], json.JSONEncoder().encode(p), jsqueue[3])
     return json.JSONDecoder().decode(gzip.GzipFile(fileobj=(StringIO.StringIO(conn.getresponse().read()))).read())["result"]
 
-#Remove a song from the browser queue, used to imitate a browser, in conjunction with the one above.
+
+#Remove a song from the browser queue
+#used to imitate a browser, in conjunction with the one above.
 def removeSongsFromQueue(songQueueID, userRemoved = True):
     p = {}
     p["parameters"] = {}
@@ -229,7 +231,9 @@ def removeSongsFromQueue(songQueueID, userRemoved = True):
     conn.request("POST", "/more.php?" + p["method"], json.JSONEncoder().encode(p), jsqueue[3])
     return json.JSONDecoder().decode(gzip.GzipFile(fileobj=(StringIO.StringIO(conn.getresponse().read()))).read())["result"]
 
-#Mark the song as being played more then 30 seconds, used if the download of a songs takes a long time.
+
+#Mark the song as being played more then 30 seconds
+#used if the download of a songs takes a long time.
 def markStreamKeyOver30Seconds(songID, songQueueID, streamServer, streamKey):
     p = {}
     p["parameters"] = {}
@@ -246,6 +250,7 @@ def markStreamKeyOver30Seconds(songID, songQueueID, streamServer, streamKey):
     conn = httplib.HTTPConnection(URL)
     conn.request("POST", "/more.php?" + p["method"], json.JSONEncoder().encode(p), jsqueue[3])
     return json.JSONDecoder().decode(gzip.GzipFile(fileobj=(StringIO.StringIO(conn.getresponse().read()))).read())["result"]
+
 
 #Mark the song as downloaded, hopefully stopping us from getting banned.
 def markSongDownloadedEx(streamServer, songID, streamKey):
@@ -275,8 +280,8 @@ def markSongDownloadedEx(streamServer, songID, streamKey):
     try:
         ret = res['result']['Return']
     except Exception as exc:
-        logging.warning("Unexpected format for markDownloaded")
-        pprint(ret)
+        logging.exception("Unexpected format for markDownloaded")
+        logging.debug(pformat(ret))
         return
 
     if ret is not True:
@@ -287,26 +292,29 @@ def ui_results(query):
     s = getResultsFromSearch(query)
     res_to_show = 30
     l = [
-            ('%d: "%s" by "%s" (%s) {ID=%s}' % \
-                    (i, l["SongName"], l["ArtistName"], l["AlbumName"],
-                        l['SongID']))
-            for i,l in enumerate(s[:res_to_show])
-            ] # output strings for first N entries
-    if l == []: #If the result was empty print a message and exit
+        ('%d: "%s" by "%s" (%s) {ID=%s}' %
+         (i, l["SongName"], l["ArtistName"], l["AlbumName"],
+          l['SongID']))
+        for i, l in enumerate(s[:res_to_show])
+    ]  # output strings for first N entries
+    if not l:  # If the result was empty print a message and exit
         print "No results found"
         exit(1)
     else:
         print '\n'.join(l)
     songid = raw_input("Enter the Song IDs you wish to download (separated with commas) or (q) to exit: ")
-    if songid == "" or songid == "q": exit() #Exit if choice is empty or q
-    inputtedIDs=songid.split(',')
+    if songid == "" or songid == "q":
+        exit()
+    inputtedIDs = songid.split(',')
 
     for curID in inputtedIDs:
         yield s[int(curID.strip())]
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    print entrystring
+    if os.getenv('GROOVE_DEBUG') == '1':
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
     getToken()
     queueID = getQueueID()
     if '#!/playlist/' in sys.argv[1]:
@@ -320,13 +328,14 @@ if __name__ == "__main__":
     elif '#!/' in sys.argv[1]:
         #provamoce
         name = sys.argv[1].rsplit('/', 1)[1]
-        logging.debug("Trying " + name)
         item = getItemFromPage(name)
-        pprint(item)
-        if 'artist' in item:
+        if not item['type']:
+            logging.error("Cannot find '%s'" % name)
+            sys.exit(1)
+        elif item['type'] == 'artist':
             songs = getArtistSongs(item['artist']['ArtistID'])
         else:
-            logging.error("Unsupported item type")
+            logging.error("Unsupported item type: %s" % item['type'])
             logging.debug(pformat(item))
             sys.exit(1)
     else:
@@ -337,36 +346,42 @@ if __name__ == "__main__":
         else:
             filename = "%s - %s.mp3" % (song["ArtistName"], song["Name"])
         if os.path.exists(filename):
-            print "File '%s' already exists; skipping" % filename
+            logging.info("File '%s' already exists; skipping" % filename)
             continue
         addSongsToQueue(song, queueID)
         stream = getStreamKeyFromSongIDs(song["SongID"])
-        for k,v in stream.iteritems():
-            stream=v
-        if stream == []:
-            print "Failed"
-            exit()
+        for k, v in stream.iteritems():
+            stream = v  # wtf
+        if not stream:
+            logging.critical("Cannot retrieve stream url for song")
+            sys.exit(1)
         cmd = 'wget --post-data=streamKey=%s -O "%s" "http://%s/stream.php"' % \
-                (stream["streamKey"],
-                        filename,
-                        stream["ip"]) #Run wget to download the song
+              (stream["streamKey"],
+               filename,
+               stream["ip"])
         p = subprocess.Popen(cmd, shell=True)
         #Starts a timer that reports the song as being played for over 30-35 seconds. May not be needed.
         markTimer = threading.Timer(30 + random.randint(0, 5),
-                markStreamKeyOver30Seconds,
-                [song["SongID"], str(queueID), stream["ip"], stream["streamKey"]]
-                )
+                                    markStreamKeyOver30Seconds,
+                                    [song["SongID"],
+                                     str(queueID),
+                                     stream["ip"],
+                                     stream["streamKey"]
+                                     ]
+                                    )
         markTimer.start()
 
         try:
-            p.wait() #Wait for wget to finish
-        except KeyboardInterrupt: #If we are interrupted by the user
+            p.wait()  # Wait for wget to finish
+        except KeyboardInterrupt:  # If we are interrupted by the user
             try:
-                os.remove(filename) #Delete the song
-                print "\nDownload cancelled. File deleted."
+                os.remove(filename)
+                logging.info("\nDownload cancelled. File deleted.")
             except:
                 pass
 
         markTimer.cancel()
         logging.debug("Marking song as completed")
-        markSongDownloadedEx(stream["ip"], song["SongID"], stream["streamKey"]) #This is the important part, hopefully this will stop grooveshark from banning us.
+        #This is the important part
+        #hopefully this will stop grooveshark from banning us.
+        markSongDownloadedEx(stream["ip"], song["SongID"], stream["streamKey"])
